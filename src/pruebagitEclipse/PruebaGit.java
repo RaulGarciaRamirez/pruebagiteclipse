cb3979500bbebb4146490dfbeda1bb515a1840b2
Raúl <raulgr74@educastur.es>
2021-11-05 13:45:25 +0100

    Segundo commit

--- src/pruebagitEclipse/PruebaGit.java
+++ src/pruebagitEclipse/PruebaGit.java
@@ -3,7 +3,8 @@ package pruebagitEclipse;
 public class PruebaGit {
 
 	public static void main(String[] args) {
-		System.out.print("lo que querais");
+		System.out.println("lo que querais");
+		System.out.print("prueba 2");
 
 	}
 
