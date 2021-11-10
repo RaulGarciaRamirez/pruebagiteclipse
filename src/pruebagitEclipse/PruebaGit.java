cdbb19e7f65914cf09c6a170e6d4dbe1f95f72fd
Raúl <raulgr74@educastur.es>
2021-11-10 13:06:32 +0100

    Revert "Tercer commit"
    This reverts commit 943c6b82eb615c0cf927cf65fe4e5bf1e6dce7f0.

--- src/pruebagitEclipse/PruebaGit.java
+++ src/pruebagitEclipse/PruebaGit.java
@@ -4,8 +4,7 @@ public class PruebaGit {
 
 	public static void main(String[] args) {
 		System.out.println("lo que querais");
-		System.out.println("prueba 2");
-		System.out.println("prueba 3");
+		System.out.print("prueba 2");
 
 	}
 
